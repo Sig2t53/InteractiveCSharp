6e7c55c684ee9efb79a993c3cda762144d4b7ae1
Seigo Itou <seigo2t53@gmail.com>
2019-03-28 00:04:07 +0900

    P.19 問題を実装 ※コンパイルエラーになる

---
+++ ConsEx2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsEx2
+{
+    class ConsEx2
+    {
+        int i;
+        public ConsEx2(int i)
+        {
+            this.i = i;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***コンストラクタを使った実験***");
+            ConsEx2 ob2 = new ConsEx2();
+        }
+    }
+}
