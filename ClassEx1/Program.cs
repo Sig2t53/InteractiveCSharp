80977181051cba77ddbe9b4a190e9bdab5df38fe
Seigo Itou <seigo2t53@gmail.com>
2019-03-27 23:56:02 +0900

    P.16 リスト2-2まで完了

--- ClassEx1/Program.cs
+++ ClassEx1/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace ClassEx1
 {
+    class ClassEx1
+    {
+        //フィールドの初期化は必須ではない
+        public int MyInt = 25;
+        //public int MyInt;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*** 2つのオブジェクトを作成するデモ ***");
+            ClassEx1 obA = new ClassEx1();
+            ClassEx1 obB = new ClassEx1();
+            Console.WriteLine("obA.i = {0}" ,obA.MyInt);
+            Console.WriteLine("obB.i = {0}" ,obB.MyInt);
+            Console.ReadKey();
         }
     }
 }
---
+++ DefaultConstructorCaseStudy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultConstructorCaseStudy
+{
+    class DefConsDemo
+    {
+        public int myInt;
+        public float myFloat;
+        public double myDouble;
+        public DefConsDemo()
+        {
+            Console.WriteLine("意図的に初期化しています");
+            myInt = 10;
+            myFloat = 0.123456F;
+            myDouble = 9.8765432;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** ユーザ定義とC#既定のコンストラクタの比較 ***\n");
+            DefConsDemo ObDef = new DefConsDemo();
+            Console.WriteLine("myInt={0}",ObDef.myInt);
+            Console.WriteLine("myFloat={0}",ObDef.myFloat);
+            Console.WriteLine("myDouble={0}", ObDef.myDouble);
+            Console.Read();
+        }
+    }
+}
