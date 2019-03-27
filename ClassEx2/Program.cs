75994103447ebd79bf137a3f2e3b555fc88f001d
Seigo Itou <seigo2t53@gmail.com>
2019-03-28 00:12:30 +0900

    P.22 リスト2-3：オブジェクト作成時に異なる値で初期化

---
+++ ClassEx2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEx2
+{
+    class ClassA
+    {
+        public int i;
+        public ClassA(int i)
+        {
+            this.i = i;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***２つのオブジェクトを作成するデモ***");
+            ClassA obA = new ClassA(10);
+            ClassA obB = new ClassA(20);
+            Console.WriteLine("obA.i =" + obA.i);
+            Console.WriteLine("obB.i =" + obB.i);
+            Console.ReadKey();
+        }
+    }
+}
