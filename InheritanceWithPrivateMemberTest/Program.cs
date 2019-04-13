fac89b42e149cd1c852d213f83e8f4d52310e870
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 01:34:32 +0900

    P.37 リスト3-2:privateなメンバの継承

---
+++ InheritanceWithPrivateMemberTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceWithPrivateMemberTest
+{
+    class A
+    {
+        private int a;
+    }
+    class B : A { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B obB = new B();
+            A obA = new A();
+            //これはaも継承されている証拠を示すものです。エラーメッセージを見てください
+            Console.WriteLine(obB.a);
+            // 「A.aはアクセスできない保護レベル」というメッセージが出る
+            Console.WriteLine(obB.b);
+            // 「Bにbの定義が含まれておらず…」というメッセージが出る
+            Console.WriteLine(obA.b);
+            // 「Aにbの定義が含まれておらず…」というメッセージが出る
+        }
+    }
+}
