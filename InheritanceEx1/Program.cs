e03afa98ae38e22e2cf76fcbe7ab713913fa356b
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 01:24:38 +0900

    P.35 リスト3-1:継承のサンプル

---
+++ InheritanceEx1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceEx1
+{
+    class ParentClass
+    {
+        public void ShowParent()
+        {
+            Console.WriteLine("親クラスの内容");
+        }
+    }
+    class ChildClass : ParentClass
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** 継承のテスト ***\n\n");
+            ChildClass child1 = new ChildClass();
+            //ChildClassオブジェクトを通じてShowParent()を呼び出す
+            child1.ShowParent();
+            Console.ReadKey();
+        }
+    }
+}
