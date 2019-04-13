615421e87d2a13ba291a15136f909e98e71a0e91
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 01:43:36 +0900

    P.39 リスト3-3:メソッドの再定義(オーバーライド)、多重継承

---
+++ MultipleInheritanceError/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritanceError
+{
+    class Parent
+    {
+        public void Show()
+        {
+            Console.WriteLine("私はParentクラスにいます");
+        }
+    }
+
+    class Child1 : Parent
+    {
+        public void Show()
+        {
+            Console.WriteLine("私はChild-1クラスにいます");
+        }
+    }
+
+    class Child2 : Parent
+    {
+        public void Show()
+        {
+            Console.WriteLine("私はChild-2クラスにいます");
+        }
+    }
+
+    class GrandChild : Child1,Child2 //菱形継承問題によってエラーとなる
+    {
+        public void Show()
+        {
+            Console.WriteLine("私はGrandChildクラスにいます");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
