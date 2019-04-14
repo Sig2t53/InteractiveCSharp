24166ae1fe0b74f2f15dd7fb39ca5a728a2089e7
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 11:24:07 +0900

    P.44 問題

---
+++ FieldInitializationOrderEx1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldInitializationOrderEx1
+{
+    class A
+    {
+        int x = 10;
+        //↓のコメントを外すとエラーになることを確認する
+        //int y = x + 2; //エラー
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** C#のフィールド初期化順序を理解する ***");
+            int x = 10;
+            int y = x + 2; //OK
+            Console.WriteLine("x={0}", x);
+            Console.WriteLine("y={0}", y);
+        }
+    }
+}
