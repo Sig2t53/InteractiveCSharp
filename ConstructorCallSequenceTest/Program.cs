f453ba4a2d7499716cf89f74bf767fe1c931fdf2
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 11:17:35 +0900

    P.40 リスト3-4:コンストラクタの呼び出し経路,P.42 リスト3-5:baseの使い方

---
+++ ConstructorCallSequenceTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorCallSequenceTest
+{
+    class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("私はParentのコンストラクタにいます。");
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child()
+        {
+            Console.WriteLine("私はChildのコンストラクタにいます。");
+        }    
+    }
+
+    class GrandChild : Child
+    {
+        public GrandChild()
+        {
+            Console.WriteLine("私はGrandChildのコンストラクタにいます。");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** コンストラクタの呼び出す順序をテスト ***");
+            GrandChild grandChild = new GrandChild();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UseOfbaseKeywordEx1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseOfbaseKeywordEx1
+{
+    class Parent
+    {
+        private int a;
+        private int b;
+        public Parent(int a, int b)
+        {
+            Console.WriteLine("私はParentのコンストラクタにいます。");
+            Console.WriteLine("インスタンス変数のaとbの値を設定");
+            this.a = a;
+            this.b = b;
+            Console.WriteLine("a={0}", this.a);
+            Console.WriteLine("b={0}", this.b);
+        }
+    }
+
+    class Child : Parent
+    {
+        private int c;
+        public Child(int a, int b, int c) : base(a,b)
+        {
+            Console.WriteLine("私はChildのコンストラクタにいます。");
+            Console.WriteLine("インスタンス変数cの値を設定");
+            this.c = c;
+            Console.WriteLine("c={0}",this.c);
+        }
+    } 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** baseキーワードの使い方の確認例 その1 ***\n\n");
+            Child obChild = new Child(1, 2, 3);
+            // Console.WriteLine("a in ObB2={0}",obChild.a);
+            // aはprivateなのでChild.aにはアクセスできない
+            Console.ReadKey();
+        }
+    }
+}
