ea87ad80cdde679eb0f4f13348caaa0a9c039e51
Seigo Itou <seigo2t53@gmail.com>
2019-04-15 00:24:45 +0900

    P.56～57 リスト4-6:Mainのオーバーロード、図4-4:Mianメソッドの追加

---
+++ OverloadingMainEx/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMainEx
+{
+    class Program
+    {
+        // 本ソースコードはコンパイル・実行はできるが、
+        // 「Program.Main(int)' で間違った認証が使われています。エントリ ポイントとして使用することはできません。」
+        // という警告が出力される
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Main()のオーバーロードのテスト ***");
+            Console.WriteLine("Main(string[] args)の中にいます");
+            Console.WriteLine("オーバーロードされたMainを呼び出しています");
+            Main(5);
+            Console.WriteLine("*** メソッドオーバーロードの考え方 ***\n\n");
+            Console.ReadKey();
+        }
+        static void Main(int a)
+        {
+            Console.WriteLine("Main(int a)の中にいます");
+        }
+
+        // ↓は追加するとエラーになる
+        //   プログラムのエントリポイントにはMain()またはMain(string[] args)を使うことができるが
+        //   両方が存在するとコンパイラがどちらを使うか判断できなくなるため。
+        //static void Main()
+        //{
+        //    Console.WriteLine("Main()の中にいます。");
+        //}
+    }
+}
