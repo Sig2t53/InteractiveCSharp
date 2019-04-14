e85d67c8e9df47704d7cd1b02b0bbecf32a804d7
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 20:30:31 +0900

    P.52～53 リスト4-1:メソッドのオーバーロード、リスト4-2,リスト4-3:正しいメソッドのオーバーロード例？

---
+++ OverloadingEx1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingEx1
+{
+    class OverloadEx1
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+
+        public double Add(double x,double y)
+        {
+            return x + y;
+        }
+
+        public string Add(string s1, string s2)
+        {
+            return string.Concat(s1,s2);
+
+        }
+    }
+
+    class OverloadEx1_List4_2
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int Add(int x, int y,int z)
+        {
+            return x + y + z;
+        }
+    }
+
+
+    // ↓誤ったオーバーロードの例 戻り値の型はメソッドシグネチャの一部とはみなされない
+    //class OverloadEx1_List4_3
+    //{
+    //    public int Add(int x, int y)
+    //    {
+    //        return x + y;
+    //    }
+    //
+    //    public double Add(int x, int y)
+    //    {
+    //        return x + y;
+    //    }
+    //}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** メソッドのオーバーロードという考え方 ***");
+            OverloadEx1 ob = new OverloadEx1();
+            Console.WriteLine("2+3={0}",ob.Add(2,3));
+            Console.WriteLine("20.5+30.7={0}",ob.Add(20.5, 30.7));
+            Console.WriteLine("Amit + Bose={0}",ob.Add("Amit","Bose"));
+            Console.ReadKey();
+        }
+    }
+}
