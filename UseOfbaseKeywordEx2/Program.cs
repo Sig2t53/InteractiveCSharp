6d0855899494a1bd19506293361987f5f49aeb3a
Seigo Itou <seigo2t53@gmail.com>
2019-04-14 11:34:44 +0900

    P.45 リスト3-6:baseを通して派生クラスから親クラスのメソッドを呼び出す

---
+++ UseOfbaseKeywordEx2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseOfbaseKeywordEx2
+{
+    // 言語仕様で、基本クラスへのアクセスは
+    //   - コンストラクタ内部
+    //   - インスタンスメソッド内部
+    //   - インスタンスプロパティへのアクセス時
+    // にのみ許可される。
+    // 静的メソッドの中でbaseキーワードを使った処理はできない。
+    class Parent
+    {
+        public void ParentMethod()
+        {
+            Console.WriteLine("私はParenetのメソッドにいます。");
+        }
+    }
+    class Child : Parent
+    {
+        public void ChildMethod()
+        {
+            Console.WriteLine("私はChildのメソッドにいます");
+            Console.WriteLine("今 Parentのメソッドを呼び出しています。");
+            base.ParentMethod();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** baseキーワードの使い方の確認例 その2 ***\n");
+            Child obChild = new Child();
+            obChild.ChildMethod();
+            Console.ReadKey();
+        }
+    }
+}
