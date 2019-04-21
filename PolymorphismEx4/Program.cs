0c27dbaf10136d1988334a1f9ec04a84e2208b81
Seigo Itou <seigo2t53@gmail.com>
2019-04-21 21:52:24 +0900

    P.74 リスト4-16:sealedを使う

---
+++ PolymorphismEx4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismEx4
+{
+
+    // 子クラスによる継承の禁止のサンプル
+    // ChildClassのコメントを外すと
+    // 「'ChildClass': シールド型 'ParentClass' から派生することはできません。」が発生する
+    sealed class ParentClass
+    {
+        public void ShowClassName()
+        {
+            Console.WriteLine("Parent.ShowClassNameの中にいます");
+        }
+    }
+
+    //class ChildClass : ParentClass
+    //{
+    //    //何かのコード
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
