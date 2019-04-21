df9433e5046bc71ea737c0918d42b3905d2e75fd
Seigo Itou <seigo2t53@gmail.com>
2019-04-21 22:00:15 +0900

    P.75 リスト4-17:キーワードをメソッドに使う

---
+++ PolymorphismEx5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismEx5
+{
+    // メソッドのオーバーライドの禁止のサンプル
+    // GrandChildClassのShowClassNameのコメントを外すと
+    // 「'GrandChildClass.ShowClassName()': 継承されたメンバー 'ChildClass.ShowClassName()' はシールドされているため、オーバーライドできません。」が発生する
+    class ParentClass
+    {
+        public virtual void ShowClassName()
+        {
+            Console.WriteLine("Parent.ShowClassNameの中にいます");
+        }
+    }
+
+    class ChildClass : ParentClass
+    {
+        sealed public override void ShowClassName()
+        {
+            Console.WriteLine("ChildClass.ShowClassNameの中にいます");
+        }
+    }
+
+    class GrandChildClass : ChildClass
+    {
+        //public override void ShowClassName()
+        //{
+        //    Console.WriteLine("GrandChildClass.ShowClassNameの中にいます");
+        //}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
