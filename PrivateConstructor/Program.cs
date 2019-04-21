9bc978901ff4f5fada8eeec3870dc6b9cc6aeac0
Seigo Itou <seigo2t53@gmail.com>
2019-04-22 00:28:38 +0900

    p.76 リスト4-18:privateなコンストラクタ

---
+++ PrivateConstructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateConstructor
+{
+
+    // ParentClassのコンストラクタがprivateになっているため、子クラスを作成できない
+    // ChildClassのコメントを外すとエラーになる
+    // ⇒ singleton(※)デザインパターンを作るときに使える
+    //    ※singletonデザインパターン:システム内に不要なオブジェクトを作れない(newできない)ようにするパターン
+    //'ParentClass.ParentClass()' はアクセスできない保護レベルになっています PrivateConstructor
+
+    class ParentClass
+    {
+        private ParentClass() { }
+        public void ShowClassName()
+        {
+            Console.WriteLine("Parent.ShowClassNameの中にいます");
+        }
+    }
+
+    //class ChildClass : ParentClass
+    //{
+
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
