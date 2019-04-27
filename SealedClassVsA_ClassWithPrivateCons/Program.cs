a60c33c9cb2dcdad924458d010fadcf2de4c26bd
Seigo Itou <seigo2t53@gmail.com>
2019-04-28 00:17:59 +0900

    P.79 リスト4-20:ケース1をブラッシュアップする

---
+++ SealedClassVsA_ClassWithPrivateCons/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClassVsA_ClassWithPrivateCons
+{
+    //メンバ変数にはsealedは適用できない
+    //⇒「コンパイルエラー 修飾子 'sealed' がこの項目に対して有効ではありません。」が発生する
+    //  変更させたくない場合はreadonlyにする
+    //class A
+    //{
+    //    sealed int a = 5;
+    //}
+    //Constructorにもsealedは適用できない
+    //⇒「コンパイルエラー 修飾子 'sealed' がこの項目に対して有効ではありません。」が発生する
+    //   子クラスから親クラスを呼び出せないようにしたいのであればprivateを使う
+    //class A
+    //{
+    //    sealed A()
+    //    {
+    //
+    //    }
+    //}
+
+    // 継承を防ぐためにはCase1,2どちらの書き方がよいか
+    // ⇒何をしたいのか明確にする。
+    //     Case1は少し手を加えれば新しい派生クラスの生成が可能
+    //     Case2は派生クラスが作れない
+    ////case1
+    //class a1
+    //{
+    //    private a1() { }
+    //}
+    //
+    ////case2
+    //sealed class a2
+    //{
+    //    //何かのコード
+    //}
+    //
+    // 継承を防ぐためにConstructorをprivateにするのは間違い
+    // privateを適用したConstructorはstaticなメンバだけを含むクラスで使われる
+    // ⇒singletonパターン(追加でのインスタンス化の防止)
+
+    class A1
+    {
+        public int x;
+        private A1() { }
+        public A1(int x) { this.x = x; }
+    }
+    sealed class A2
+    {
+        // 何かのコード
+    }
+    class B1 : A1
+    {
+        public int y;
+        public B1(int x,int y):base(x)
+        {
+            this.y = y;
+        }
+    }
+    //キーワードsealedがついているA2型から子クラスは派生不可
+    //↓を有効にするとコンパイルエラー「'B2': シールド型 'A2' から派生することはできません。」が発生する
+    //class B2 : A2 { }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** ケーススタディ : クラスにsealedをつけるかコンストラクタにprivateをつけるか ***\n");
+            B1 obB1 = new B1(2, 3);
+            Console.WriteLine("\t x={0}", obB1.x);
+            Console.WriteLine("\t x={0}", obB1.y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SealedClassVsA_ClassWithPrivateCons/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("SealedClassVsA_ClassWithPrivateCons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SealedClassVsA_ClassWithPrivateCons")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("159ddb0c-4e08-4c9b-ba1a-75bcfe65fb4a")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
