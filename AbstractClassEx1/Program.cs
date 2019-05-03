4ff2065d1f6275a04608e6d02954110e1ef46e5f
Seigo Itou <seigo2t53@gmail.com>
2019-05-04 01:36:30 +0900

    P.86 リスト4-24:抽象クラスを使う

---
+++ AbstractClassEx1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassEx1
+{
+    //抽象メソッドを1つでも含むクラスは抽象クラスであることを明記しなくてはならない
+    abstract class MyAbstractClass
+    {
+        public abstract void ShowMe();
+    }
+
+    class MyConcreteClass : MyAbstractClass
+    {
+        public override void ShowMe()
+        {
+            Console.WriteLine("私は実装のあるクラスから来ました");
+            Console.WriteLine("私のShowMe()メソッドの内容は完結しています");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** 抽象クラスの例 その1 ***\n");
+            //↓ エラー 抽象クラスからはインスタンス作成できない
+            //「抽象クラスまたはインターフェイス 'MyAbstractClass' のインスタンスを作成できません。」
+            // MyAbstractClass abstractOb = new MyAbstractClass();
+            MyConcreteClass concreteOb = new MyConcreteClass();
+            concreteOb.ShowMe();
+            Console.ReadKey();
+        }
+    }
+}
