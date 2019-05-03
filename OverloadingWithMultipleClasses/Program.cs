c74d2b4a653cb23f67e899a419741ab89f89e623
Seigo Itou <seigo2t53@gmail.com>
2019-05-03 20:11:22 +0900

    P.81 リスト4-21:複数のクラスにまたがるオーバーロード

---
+++ OverloadingWithMultipleClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingWithMultipleClasses
+{
+    class Parent
+    {
+        public void ShowMe()
+        {
+            Console.WriteLine("Parent.ShowMe その１:引数なし");
+        }
+        public void ShowMe(int a)
+        {
+            Console.WriteLine("Parent.ShowMe その２:整数型の引数１つ");
+        }
+    }
+    class Child : Parent
+    {
+        // 子クラスや派生クラスにおけるオーバーロード型
+        public void ShowMe(int a, int b)
+        {
+            Console.WriteLine("Chld.ShowMe その３:整数型の引数２つ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** 複数のクラスにまたがるオーバーロード ***\n");
+            Child childOB = new Child();
+            //3つのオーバーロード型をすべて呼び出す
+            childOB.ShowMe();
+            childOB.ShowMe(1);
+            childOB.ShowMe(1, 2);
+            Console.ReadKey();
+        }
+    }
+}
