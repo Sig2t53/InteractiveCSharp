ec5df8a9026ccc913341c4cbde873b8adcde7de9
Seigo Itou <seigo2t53@gmail.com>
2019-05-04 02:18:24 +0900

    P.89 リスト4-26:抽象メソッドのない抽象クラス

---
+++ AbstractClassEx3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassEx3
+{
+    abstract class MyabstractClass
+    {
+        protected int myInt = 25;
+        //public abstract void ShowMe();
+        public virtual void CompleteMethod1()
+        {
+            Console.WriteLine("MyabstractClass.CompleteMethod1()");
+        }
+        public void CompleteMethod2()
+        {
+            Console.WriteLine("MyabstractClass.CompleteMethod2()");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** 未実装のない抽象クラスのコンパイル***");
+            //↓抽象クラスはインスタンスを生成できない
+            //「抽象クラスまたはインターフェイス 'MyabstractClass' のインスタンスを作成できません。」
+            // が発生する
+            //MyabstractClass abstractOb = new MyabstractClass();
+            Console.ReadKey();
+        }
+    }
+}
