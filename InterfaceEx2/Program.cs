a3d841ba85a4e800694c4f84a5205d8e5bb3bbec
Seigo Itou <seigo2t53@gmail.com>
2019-05-04 13:04:27 +0900

    P.98 リスト5-2:エラーを出さないインターフェース P99 リスト5-3:2つ以上のインターフェースの例

---
+++ InterfaceEx2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceEx2
+{
+    interface IMyInterface
+    {
+        void Show1();
+        void Show2();
+    }
+
+
+    // MyClassはインターフェース;IMyInterfaceのShow2メソッドを実装していないので抽象クラスになる
+    abstract class MyClass : IMyInterface
+    {
+        public void Show1()
+        {
+            Console.WriteLine("MyClass.Show1()が実装されている");
+        }
+
+        abstract public void Show2();
+    }
+
+    class ChildClass : MyClass
+    {
+        public override void Show2()
+        {
+            Console.WriteLine("ChildClass.Show2()を実装したので完璧");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** インターフェースを研究する例 その2 ***\n");
+            // MyClassは抽象クラス
+            // MyClass myClassOb = new MyClass();
+            MyClass myOb = new ChildClass();
+            myOb.Show1();
+            myOb.Show2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ InterfaceEx3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceEx3
+{
+    interface IMyInterface3A
+    {
+        void Show3A();
+    }
+
+    interface IMyInterface3B
+    {
+        void Show3B();
+    }
+    class MyClass3 : IMyInterface3A, IMyInterface3B
+    {
+        public void Show3A()
+        {
+            Console.WriteLine("MyClass3.Show3A()が実装されました");
+        }
+
+        public void Show3B()
+        {
+            Console.WriteLine("MyClass3.Show3B()が実装されました");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** インターフェースを研究する例 その3 ***\n");
+            MyClass3 myClassOb = new MyClass3();
+            myClassOb.Show3A();
+            myClassOb.Show3B();
+            Console.ReadKey();
+        }
+    }
+}
