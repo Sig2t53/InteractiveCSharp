59f69ea9a11070ab7a9652d4cf1ad4fd14c66cf3
Seigo Itou <seigo2t53@gmail.com>
2019-05-04 12:20:20 +0900

    P.96 リスト5-1:インターフェースのサンプル

---
+++ InterfaceEx1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceEx1
+{
+    interface IMyInterface
+    {
+        void Show();
+    }
+
+    class MyClass : IMyInterface
+    {
+        //インターフェースの実装においてメソッドシグネチャは一致している必要がある
+        public void Show()
+        {
+            Console.WriteLine("MyClass.Show()を実装しました");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** インターフェースを研究する例 その1 ***\n");
+            MyClass myClassOb = new MyClass();
+            myClassOb.Show();
+            Console.ReadKey();
+        }
+    }
+}
