e920c95f222d0f0c46821ce608ec2c56976477cd
Seigo Itou <seigo2t53@gmail.com>
2019-05-07 22:59:11 +0900

    P.121 リスト6‒3:virtual の使い方

---
+++ VirtualPropertyEx1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPropertyEx1
+{
+    class Shape
+    {
+        public virtual double Area
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+
+    class Circle : Shape
+    {
+        int radius;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public int Radius
+        {
+            get
+            {
+                return radius;
+            }
+        }
+        public override double Area
+        {
+            get
+            {
+                return 3.14 * radius * radius;
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** virtualなプロパティのケーススタディ ***");
+            Circle myCircle = new Circle(10);
+            Console.WriteLine("\n円の半径は{0}単位",myCircle.Radius);
+            Console.WriteLine("円の面積は{0}兵法単位",myCircle.Area);
+            Console.ReadKey();
+        }
+    }
+}
