2d51b0eeb6282e45d7b91344a7c7b3eb38f27ce5
Seigo Itou <seigo2t53@gmail.com>
2019-06-22 11:38:49 +0900

    P.139 リスト7-2:静的メソッドとインスタンスメソッドの両方を含む

---
+++ StaticClassEx1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassEx1
+{
+    class Program
+    {
+        static class Rectangle
+        {
+            public static double Area(double len ,double bre)
+            {
+                return len * bre;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** クラス変数の例　その１ ***\n");
+            double length = 25;
+            double breadth = 10;
+            Console.WriteLine("長方形の面積は{0}平方単位",Rectangle.Area(length,breadth));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StaticMethodsEx1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsEx1
+{
+    class NonStaticClass
+    {
+        public static void StaticMethod()
+        {
+            Console.WriteLine("非静的クラスの静的メソッド");
+        }
+        public void NonStaticMethod()
+        {
+            Console.WriteLine("非静的クラスの非静的(インスタンス)メソッド");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** 静的メソッドの実装例 その１ ***\n");
+            NonStaticClass anObject = new NonStaticClass();
+            anObject.NonStaticMethod();
+            //anObject.StaticMethod(); //エラー CS0176  インスタンス参照でメンバー 'NonStaticClass.StaticMethod()' にアクセスできません。代わりに型名を使用してください
+            NonStaticClass.StaticMethod();
+            Console.ReadKey();
+        }
+    }
+}
