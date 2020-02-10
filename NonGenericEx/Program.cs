c8cdeb4acdc58cec8876bd65737b4dda88b32143
Seigo Itou <seigo2t53@gmail.com>
2020-02-11 02:06:01 +0900

    P.223 リスト12-1:ジェネリックを使わない従来のプログラム

---
+++ NonGenericEx/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericEx
+{
+    class NonGenericEx
+    {
+        public int ShowInteger(int i)
+        {
+            return 1;
+        }
+        public string ShowString(string s1)
+        {
+            return s1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**ジェネリックを使わないプログラム**");
+            NonGenericEx nonGenericOb = new NonGenericEx();
+            Console.WriteLine("ShowIntegerの戻り値は{0}",nonGenericOb.ShowInteger(25));
+            Console.WriteLine("ShowStringの戻り値は{0}",nonGenericOb.ShowString("ジェネリックではないメソッドが呼ばれました"));
+            Console.ReadKey();
+        }
+    }
+}
