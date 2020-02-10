a8f5ca34a5e1a2e7e066b4fc9b37a023271b4226
Seigo Itou <seigo2t53@gmail.com>
2020-02-11 02:50:44 +0900

    P.225 リスト12-2:リスト12-1をジェネリックを使って書き換える

---
+++ GenericProgrammingEx1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProgrammingEx1
+{
+    class MyGenericClass<T>
+    {
+        public T Show(T value)
+        {
+            return value;
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** ジェネリック入門 **");
+            MyGenericClass<int> myGenericClassIntOb = new MyGenericClass<int>();
+            Console.WriteLine("Showの戻り値は{0}",myGenericClassIntOb.Show(100));
+            MyGenericClass<string> myGenericClassStringOb = new MyGenericClass<string>();
+            Console.WriteLine("Showの戻り値は{0}",myGenericClassStringOb.Show("ジェネリックなメソッドが呼ばれました。"));
+            MyGenericClass<double> myGenericClassDoubleOb = new MyGenericClass<double>();
+            Console.WriteLine("Showの戻り値は{0}",myGenericClassDoubleOb.Show(100.5));
+            Console.ReadKey();
+        }
+    }
+}
