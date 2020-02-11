b72eecac323c8ebfaab1f9066a389d179c963d82
Seigo Itou <seigo2t53@gmail.com>
2020-02-11 10:25:21 +0900

    P.227 リスト12-3 ダウンキャストを使った実験

---
+++ GenericEx2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** ジェネリックで実行時エラーを回避 ***");
+            ArrayList myList = new ArrayList();
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add("invalid");
+
+            // コンパイルエラーはないが、実行時エラーが生じる
+            foreach(int myInt in myList)
+            {
+                Console.WriteLine((int)myInt); //ダウンキャスト
+            }
+            Console.ReadKey();
+        }
+    }
+}
