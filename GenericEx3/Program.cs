1018261103d88c9d6c2cbd1c59bec7d730d21c96
Seigo Itou <seigo2t53@gmail.com>
2020-02-11 10:33:01 +0900

    P.228 リスト12-4 リスト12-3にジェネリックを使う

---
+++ GenericEx3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEx3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** ジェネリックでランタイムエラーを回避 ***");
+            List<int> myGenericList = new List<int>();
+            myGenericList.Add(10);
+            myGenericList.Add(20);
+            // myGenericList.Add("無効");　// コンパイルエラー	CS1503 引数 1: は 'string' から 'int' へ変換することはできません。
+            foreach (int myInt in myGenericList)
+            {
+                Console.WriteLine(myInt);
+            }
+            Console.ReadKey();
+        }
+    }
+}
